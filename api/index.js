f1c5f653f936641d6458b5e45a2667f586d7ca21
Vikash-Android7500 <Vikash75.jm@gmail.com>
2024-09-15 08:28:06 +0530

    new json file

--- api/index.js
+++ api/index.js
@@ -7,7 +7,19 @@ require("dotenv").config();
 let portNo = process.env.PORT || 4000;
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+
 server.use(router);
 server.listen(portNo, () => {
-  console.log(`JSON Server is running ${portNo} (-_-)...+`);
+  console.log(`JSON Server is running ${portNo} (-_-)`);
 });
+
+
+// Export the Server API
+module.exports = server
